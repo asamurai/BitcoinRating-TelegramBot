f95590c0c506811f1212ef12e135a67d820d9e7e
aSamurai <xhc044x@gmail.com>
2017-08-05 02:39:09 +0300

    Ready to run on heroku

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const bot = require('./server');
-const {dailyCron} = require('./server/cronjob.js');
 
-dailyCron(bot).start();
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.json({ version: '1.0.0' });
+});
+
+const server = app.listen(PORT, "0.0.0.0", function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Web server started at http://%s:%s', host, port);
+});
+
+app.post('/' + bot.token, function (req, res) {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
+
+// const {dailyCron} = require('./server/cronjob.js');
+// dailyCron(bot).start();
